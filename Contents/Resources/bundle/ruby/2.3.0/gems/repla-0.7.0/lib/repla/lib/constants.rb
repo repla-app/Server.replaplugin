d89faae052813df21c14350f3c5e54760babdda6
Roben Kleene <contact@robenkleene.com>
2019-03-01 17:15:10 -0500

    Add gem (#1)
    * Add gem
    
    * Add gem

---
+++ Contents/Resources/bundle/ruby/2.3.0/gems/repla-0.7.0/lib/repla/lib/constants.rb
@@ -0,0 +1,9 @@
+module Repla
+  # Keys
+  PLUGIN_NAME_KEY = 'WC_PLUGIN_NAME'.freeze
+  SPLIT_ID_KEY = 'WC_SPLIT_ID'.freeze
+  WINDOW_ID_KEY = 'WC_WINDOW_ID'.freeze
+
+  # Directories
+  APPLESCRIPT_DIRECTORY = File.join(File.dirname(__FILE__), '../../applescript')
+end

9f8feee1e441bce9f3bc46184dc96aba6220499c
Roben Kleene <contact@robenkleene.com>
2019-03-04 16:26:47 -0500

    Setup test constants

---
+++ Contents/Resources/test/lib/test_constants.rb
@@ -0,0 +1,2 @@
+PRINT_VARIABLE_PATH = File.expand_path(File.join(File.dirname(__FILE__),
+                                                 '../data/print_variable.sh'))
--- Contents/Resources/test/tc_parent.rb
+++ Contents/Resources/test/tc_parent.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'minitest/autorun'
+requiare_relative 'lib/test_constants'
 
 # Test parent
 class TestParent  < minitest::test
   def test_parent
+
   end
 end

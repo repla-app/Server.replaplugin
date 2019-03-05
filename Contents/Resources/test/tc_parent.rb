14dc581108a47a496ad2938b4944378c239018b0
Roben Kleene <contact@robenkleene.com>
2019-03-05 15:11:25 -0500

    Make test safer

--- Contents/Resources/test/tc_parent.rb
+++ Contents/Resources/test/tc_parent.rb
@@ -10,12 +10,18 @@ class TestParent < Minitest::Test
   def test_parent
     delegate = ParentDelegate.new
     parent = Repla::Parent.new(delegate)
+    output_ran = false
     delegate.add_process_output_block do |text|
       assert_equal(text, TEST_ENV_VALUE)
+      output_ran = true
     end
+    error_ran = false
     delegate.add_process_error_block do |text|
       assert_equal(text, TEST_ENV_VALUE_TWO)
+      error_ran = true
     end
     parent.run_command(PRINT_VARIABLE_PATH, TEST_ENV)
+    assert(output_ran)
+    assert(error_ran)
   end
 end

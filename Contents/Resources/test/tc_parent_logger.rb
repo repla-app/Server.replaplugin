8fbd7386126263f82887088dbf80fe81f7334311
Roben Kleene <contact@robenkleene.com>
2019-03-06 06:39:37 -0500

    Add parent logger boilerplate

--- Contents/Resources/test/tc_parent_logger.rb
+++ Contents/Resources/test/tc_parent_logger.rb
@@ -5,6 +5,17 @@ require_relative '../lib/parent_logger'
 
 # Test parent
 class TestParent < Minitest::Test
+  def setup
+    @parent_logger = Repla::ParentLogger.new
+  end
+
+  def teardown
+    window = Repla::Window.now(@parent_logger.logger.window_id)
+    window.close
+  end
+
   def test_parent_logger
+    @parent_logger.process_output(TEST_ENV_VALUE)
+    @parent_logger.process_error(TEST_ENV_VALUE_TWO)
   end
 end

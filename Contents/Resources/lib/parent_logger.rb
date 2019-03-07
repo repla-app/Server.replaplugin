c52291321aa6414b7bfe64ffa35f1c3f078e0eeb
Roben Kleene <contact@robenkleene.com>
2019-03-07 11:46:26 -0500

    Start adding URL handling

--- Contents/Resources/lib/parent_logger.rb
+++ Contents/Resources/lib/parent_logger.rb
@@ -1,4 +1,5 @@
 require_relative '../bundle/bundler/setup'
+require 'repla'
 require 'repla/logger'
 
 # Repla
@@ -9,14 +10,24 @@ module Repla
 
     def initialize
       @logger = Repla::Logger.new
+      @view = Repla::View(@logger.window_id)
     end
 
     def process_output(text)
+      # TODO: Needs to look for URL here
       @logger.info(text)
     end
 
     def process_error(text)
+      # TODO: Needs to look for URL here
       @logger.error(text)
+    end
+
+    require 'uri'
+    def self.process_line(line)
+      if string.scan(URI.regexp)
+
+      end
     end
   end
 end

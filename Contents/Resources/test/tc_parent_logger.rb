eb72fff8f074e631d0aadce048f5fb2b07b03894
Roben Kleene <contact@robenkleene.com>
2019-03-08 11:29:31 -0500

    Update tests

--- Contents/Resources/test/tc_parent_logger.rb
+++ Contents/Resources/test/tc_parent_logger.rb
@@ -7,18 +7,24 @@ require Repla::Test::LOG_HELPER_FILE
 require_relative 'lib/test_constants'
 require_relative '../lib/parent_logger'
 
-class TestParent < Minitest::Test
+class TestParentLoggerClass < Minitest::Test
   def test_url_from_line
     good_url = 'http://www.google.com'
     line_with_good_url = "Here is a URL #{good_url}"
-    ParentLogger.url_from_line(good_url)
-    line_with_local_url = 'http://127.0.0.1'
-    bad_url = 'asdf'
+    url = ParentLogger.url_from_line(line_with_good_url)
+    assert_equal(good_url, url)
+    local_url = 'http://127.0.0.1'
+    line_with_local_url = "#{local_url} is a local URL"
+    url = ParentLogger.url_from_line(line_with_local_url)
+    assert_equal(local_url, url)
+    line_with_no_url = 'This line doesn\'t have any URLs'
+    url = ParentLogger.url_from_line(line_with_no_url)
+    assert_nil(url)
   end
 end
 
 # Test parent
-class TestParent < Minitest::Test
+class TestParentLogger < Minitest::Test
   def setup
     @parent_logger = Repla::ParentLogger.new
     @logger = @parent_logger.logger

80c7bdce319fbc363390f82bdd8266cd16d199f1
Roben Kleene <contact@robenkleene.com>
2019-03-11 14:39:34 -0400

    Add no env test

--- Contents/Resources/test/data/server.sh
+++ Contents/Resources/test/data/server.sh
@@ -4,4 +4,7 @@ set -e
 
 trap 'kill $(jobs -p)' EXIT
 
+if [[ -z "$SERVER_PATH" ]]; then
+  SERVER_PATH="."
+fi
 ruby -run -e httpd -- -p 5000 $SERVER_PATH 2>&1 | while read x; do echo $x; if [[ $x == *"WEBrick::HTTPServer#start"* ]]; then echo "Server started at http://127.0.0.1:5000"; fi done
--- Contents/Resources/test/tc_server.rb
+++ Contents/Resources/test/tc_server.rb
@@ -28,3 +28,29 @@ class TestServer < Minitest::Test
     assert_equal(result, Repla::Test::INDEX_HTML_TITLE)
   end
 end
+
+class TestServerNoEnv < Minitest::Test
+  SERVER_FILE = File.expand_path(File.join(File.dirname(__FILE__),
+                                           '../server.rb'))
+
+  def setup
+    @pid = spawn(SERVER_FILE,
+                 SERVER_PATH,
+                 chdir: Repla::Test::TEST_HTML_DIRECTORY.to_s)
+    sleep Repla::Test::TEST_PAUSE_TIME
+    window_id = Repla::Test::Helper.window_id
+    @window = Repla::Window.new(window_id)
+  end
+
+  def teardown
+    @window.close
+    Process.kill(:INT, @pid)
+  end
+
+  def test_server
+    javascript = File.read(Repla::Test::TITLE_JAVASCRIPT_FILE)
+    @window.load_file(Repla::Test::INDEX_HTML_FILE)
+    result = @window.do_javascript(javascript)
+    assert_equal(result, Repla::Test::INDEX_HTML_TITLE)
+  end
+end

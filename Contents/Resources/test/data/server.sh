7ef44d5c853f2694cf8835e4d7452c70f9bf7380
Roben Kleene <contact@robenkleene.com>
2019-03-12 19:22:26 -0400

    Re-work variables

--- Contents/Resources/test/data/server.sh
+++ Contents/Resources/test/data/server.sh
@@ -7,4 +7,4 @@ trap 'kill $(jobs -p)' EXIT
 if [[ -z "$SERVER_PATH" ]]; then
   SERVER_PATH="."
 fi
-ruby -run -e httpd -- -p 5000 $SERVER_PATH 2>&1 | while read x; do echo $x; if [[ $x == *"WEBrick::HTTPServer#start"* ]]; then echo "Server started at http://127.0.0.1:5000"; fi done
+ruby -run -e httpd -- -p 5000 $SERVER_ROOT 2>&1 | while read x; do echo $x; if [[ $x == *"WEBrick::HTTPServer#start"* ]]; then echo "Server started at http://127.0.0.1:5000"; fi done
--- Contents/Resources/test/lib/test_setup.rb
+++ Contents/Resources/test/lib/test_setup.rb
@@ -3,9 +3,12 @@ require 'repla/test'
 
 PRINT_VARIABLE_PATH = File.expand_path(File.join(File.dirname(__FILE__),
                                                  '../data/print_variable.sh'))
-SERVER_PATH = File.expand_path(File.join(File.dirname(__FILE__),
-                                         '../data/server.sh'))
-TEST_SERVER_ENV = "SERVER_PATH=\"#{Repla::Test::TEST_HTML_DIRECTORY}\"".freeze
+SERVER_COMMAND = 'server.sh'.freeze
+SERVER_DIR = File.expand_path(File.join(File.dirname(__FILE__),
+                                        '../data/'))
+SERVER_PATH = File.join(SERVER_DIR,
+                        SERVER_COMMAND)
+TEST_SERVER_ENV = "SERVER_ROOT=\"#{Repla::Test::TEST_HTML_DIRECTORY}\"".freeze
 TEST_ENV_KEY = 'TEST_VARIABLE'.freeze
 TEST_ENV_KEY_TWO = 'TEST_VARIABLE_TWO'.freeze
 TEST_ENV_VALUE = 'A test string'.freeze

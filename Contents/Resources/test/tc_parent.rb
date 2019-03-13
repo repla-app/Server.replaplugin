b0fcc063617b40199b5d28aa6257b699c80f1c75
Roben Kleene <contact@robenkleene.com>
2019-03-13 13:41:22 -0400

    Fix flapping test (#5)

--- Contents/Resources/test/tc_parent.rb
+++ Contents/Resources/test/tc_parent.rb
@@ -12,15 +12,22 @@ class TestParent < Minitest::Test
     parent = Repla::Parent.new(PRINT_VARIABLE_PATH, TEST_ENV, delegate)
     output_ran = false
     delegate.add_process_output_block do |text|
+      text.chomp!
       assert_equal(text, TEST_ENV_VALUE)
       output_ran = true
     end
     error_ran = false
     delegate.add_process_error_block do |text|
+      text.chomp!
       assert_equal(text, TEST_ENV_VALUE_TWO)
       error_ran = true
     end
     parent.run
+    count = 0
+    until (error_ran && output_ran) || count > 10
+      sleep(1)
+      count += 1
+    end
     assert(output_ran)
     assert(error_ran)
   end

8e4da1c3eb3685ee9ddda0d9d6cadcb967dcd8ab
Roben Kleene <contact@robenkleene.com>
2019-03-21 15:37:32 -0400

    Add CLI test

---
+++ Contents/Resources/test/tc_cli.rb
@@ -0,0 +1,29 @@
+#!/System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/bin/ruby
+
+require 'minitest/autorun'
+
+require_relative 'lib/test_setup'
+require Repla::Test::HELPER_FILE
+
+# Test CLI
+class TestCLI < Minitest::Test
+  SYMLINK_DST = File.join(__dir__, 'repla')
+  CLI_PATH_COMPONENT = 'Contents/Resources/Scripts/repla'.freeze
+  def setup
+    bundle_command = 'osascript -e \'POSIX path of '\
+    '(path to application "Repla")\''
+    app_bundle_path = `#{bundle_command}`
+    app_bundle_path.chomp!
+    symlink_src = File.join(app_bundle_path, CLI_PATH_COMPONENT)
+    File.symlink(symlink_src, SYMLINK_DST)
+  end
+
+  def teardown
+    File.delete(SYMLINK_DST)
+  end
+
+  def test_cli
+    command = "#{SYMLINK_DST} server "\
+      "#{Repla::Test::TEST_HTML_DIRECTORY}"
+  end
+end

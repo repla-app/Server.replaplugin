30ec962cb3007ec25075d8b662444a48bd5e6ba8
Roben Kleene <contact@robenkleene.com>
2019-04-25 17:47:52 -0400

    Try fake PTY

--- Contents/Resources/lib/parent.rb
+++ Contents/Resources/lib/parent.rb
@@ -1,7 +1,5 @@
 require 'Shellwords'
 
-require 'open3'
-
 # Repla
 module Repla
   module Server
@@ -14,6 +12,52 @@ module Repla
 
       def run
         return if @command.nil?
+
+        require 'pty'
+        PTY.spawn(@command) do |stdout, stdin, pid|
+          stdin.sync = true
+          stdout.sync = true
+
+          output_thread = Thread.new do
+            stdout.each do |line|
+              @delegate.process_output(line) unless @delegate.nil?
+            end
+          end
+
+          @pid = pid
+          # Make sure all output gets processed before existing
+          stdout.flush
+          # stderr.flush
+          output_thread.join
+          error_thread.join
+        end
+
+        # require 'open3'
+        # Open3.popen3(@command) do |stdin, stdout, stderr, wait_thr|
+        #   stdin.sync = true
+        #   stdout.sync = true
+        #   stderr.sync = true
+
+        #   output_thread = Thread.new do
+        #     stdout.each do |line|
+        #       @delegate.process_output(line) unless @delegate.nil?
+        #     end
+        #   end
+
+        #   error_thread = Thread.new do
+        #     stderr.each do |line|
+        #       @delegate.process_error(line) unless @delegate.nil?
+        #     end
+        #   end
+        #   @pid = wait_thr.pid
+        #   wait_thr.value
+        #   # Make sure all output gets processed before existing
+        #   stdout.flush
+        #   stderr.flush
+        #   output_thread.join
+        #   error_thread.join
+        # end
+
 
         Open3.popen3(@command) do |stdin, stdout, stderr, wait_thr|
           stdin.sync = true

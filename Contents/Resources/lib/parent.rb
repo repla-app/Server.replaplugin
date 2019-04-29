164f4adcbbde071c4e9c8cf71a854d7ba2ececa0
Roben Kleene <contact@robenkleene.com>
2019-04-29 13:22:23 -0400

    Add comments

--- Contents/Resources/lib/parent.rb
+++ Contents/Resources/lib/parent.rb
@@ -13,6 +13,9 @@ module Repla
       def run
         return if @command.nil?
 
+        # Using `PTY`, we lose the `STDOUT` vs. `STDERR` distinction, and we
+        # have to handle escape codes, but this seems to automatically prevent
+        # any `STDOUT` buffering issues
         require 'pty'
         PTY.spawn(@command) do |stdout, stdin, pid|
           stdin.sync = true
@@ -27,10 +30,11 @@ module Repla
           @pid = pid
           # Make sure all output gets processed before existing
           stdout.flush
-          # stderr.flush
           output_thread.join
         end
 
+        # Using `open3`, we get `STDERR` and escape codes are automatically
+        # removed, by this introduces `STDOUT` buffering issues.
         # require 'open3'
         # Open3.popen3(@command) do |stdin, stdout, stderr, wait_thr|
         #   stdin.sync = true

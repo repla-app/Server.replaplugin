614023511939fac6788e0f9e33bf893be4b17d43
Roben Kleene <contact@robenkleene.com>
2019-05-09 16:04:23 -0400

    Fix shell script linter errors

--- Contents/Resources/test/data/server.sh
+++ Contents/Resources/test/data/server.sh
@@ -35,8 +35,8 @@ if [[ -z "$SERVER_ROOT" ]]; then
   SERVER_ROOT="."
 fi
 
-ruby -run -e httpd -- -p 5000 "$SERVER_ROOT" 2>&1 | while read x; do
-  echo $x
+ruby -run -e httpd -- -p 5000 "$SERVER_ROOT" 2>&1 | while read -r x; do
+  echo "$x"
   if [[ $x == *"WEBrick::HTTPServer#start"* ]]; then
     echo "Server started at http://127.0.0.1:5000";
   fi

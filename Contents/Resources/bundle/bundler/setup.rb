a25923146609795c405b808db47476f861d6002e
Roben Kleene <contact@robenkleene.com>
2019-12-04 15:05:19 -0500

    Add listener

--- Contents/Resources/bundle/bundler/setup.rb
+++ Contents/Resources/bundle/bundler/setup.rb
@@ -4,4 +4,9 @@ ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
 $:.unshift "#{path}/"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/repla-0.10.3/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-darwin-18/2.4.0-static/ffi-1.11.3"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/ffi-1.11.3/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/rb-fsevent-0.10.3/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/rb-inotify-0.10.0/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/listen-3.2.0/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/repla-0.10.4/lib"
---
+++ Contents/Resources/bundle/ruby/2.4.0/gems/ffi-1.11.3/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/robenkleene/Development/Projects/Cocoa/Repla/Packages/Server.replaplugin/Contents/Resources/bundle/ruby/2.4.0/gems/ffi-1.11.3/ext/ffi_c/libffi/src/x86/ffitarget.h

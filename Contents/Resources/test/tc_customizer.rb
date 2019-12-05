a329e19604e01349602399c7e51a74b8239d0a16
Roben Kleene <contact@robenkleene.com>
2019-12-05 15:33:51 -0500

    Add failing Jupyter test

--- Contents/Resources/test/tc_customizer.rb
+++ Contents/Resources/test/tc_customizer.rb
@@ -8,6 +8,7 @@ require_relative '../lib/customizer'
 class TestServer < Minitest::Test
   NPM_COMMAND = 'npm start'.freeze
   JUPYTER_COMMAND = 'jupyter notebook'.freeze
+  JUPYTER_COMMAND_WHITESPACE = 'jupyter     notebook'.freeze
   RAILS_COMMAND = 'bin/rails server'.freeze
   def test_customizer
     command = NPM_COMMAND
@@ -24,6 +25,12 @@ class TestServer < Minitest::Test
     command, options = Repla::Server::Customizer.customize(command)
     assert(options.empty?)
     assert_equal(RAILS_COMMAND, command)
+
+    command = JUPYTER_COMMAND_WHITESPACE
+    command, options = Repla::Server::Customizer.customize(command)
+    assert(options.empty?)
+    assert_equal(JUPYTER_COMMAND_WHITESPACE + Repla::Server::JUPYTER_SUFFIX,
+                 command)
 
     # TEST_DELAY_OPTIONS_LONG
     # TEST_OPTIONS_FILE

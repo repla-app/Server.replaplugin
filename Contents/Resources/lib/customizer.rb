5f1eddab70415fd828b161da68bb12fd23fefb7e
Roben Kleene <contact@robenkleene.com>
2019-12-19 14:55:03 -0500

    Disable refresh in home directory and library

--- Contents/Resources/lib/customizer.rb
+++ Contents/Resources/lib/customizer.rb
@@ -7,12 +7,23 @@ module Repla
       def self.customize(command, options = {})
         command = command.dup
         options = options.dup
+        options[:file_refresh] = false if disable_file_refresh?(options,
+                                                                pwd,
+                                                                home)
         command << JUPYTER_SUFFIX if customizable_jupyter?(command)
         [command, options]
       end
 
       def self.customizable_jupyter?(command)
         /jupyter\s*notebook$/.match?(command)
+      end
+
+      def self.disable_file_refresh?(options, pwd, home)
+        return false unless options[:file_refresh].nil?
+
+        return true if pwd == home
+
+        return true if pwd == File.join(home, 'Library')
       end
     end
   end
